c6b0f0b5e965ea134ac489c1e221c8316797d97a
Everson <eversonalba98@hotmail.com>
2022-09-26 11:15:19 -0500

    Examen 1

---
+++ src/main/java/pe/isil/examenuno/ExamenunoApplication.java
@@ -0,0 +1,15 @@
+package pe.isil.examenuno;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamenunoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamenunoApplication.class, args);
+
+		System.out.print("Hola Everson");
+	}
+
+}
---
+++ src/test/java/pe/isil/examenuno/ExamenunoApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.isil.examenuno;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamenunoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
